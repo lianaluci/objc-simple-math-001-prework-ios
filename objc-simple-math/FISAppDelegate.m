701f84f77657b5e2d7143f977abf4c520174e7b5
lianaluci <llgrobstein@gmail.com>
2015-11-21 00:56:03 -0500

    complete part F. exercises

--- objc-simple-math/FISAppDelegate.m
+++ objc-simple-math/FISAppDelegate.m
@@ -113,13 +113,30 @@
     y = 5 - 8 * (4 + 2);
     NSLog(@"y: %li", y);
     
+    CGFloat f = 0.0;
+    NSLog(@"f: %f", f);
+    
+    f = 17 / 29;
+    NSLog(@"f: %f", f);
+    
+    f = 17 / 29.0;
+    NSLog(@"f: %f", f);
+    
+    f = 1 + 2.5;
+    NSLog(@"f: %f", f);
+    
+    f = 10 - 1.11;
+    NSLog(@"f: %f", f);
+    
+    f = 0.1667 * 6;
+    NSLog(@"f: %f", f);
+    
+    f = 3 / 7.0;
+    NSLog(@"f: %f", f);
+
     
     
-    
-    
-    
-    
-     
+  
     
     
     // do not alter
