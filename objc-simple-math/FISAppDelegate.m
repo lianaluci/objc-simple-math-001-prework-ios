91c2854a627b3c1c279fcc1da3dc7c58cc5a28fb
lianaluci <llgrobstein@gmail.com>
2015-11-21 01:10:15 -0500

    complete ADVANCED exercises

--- objc-simple-math/FISAppDelegate.m
+++ objc-simple-math/FISAppDelegate.m
@@ -133,6 +133,29 @@
     
     f = 3 / 7.0;
     NSLog(@"f: %f", f);
+    
+    f = M_PI;
+    NSLog(@"f: %f", f);
+    
+    NSLog(@"f: %.12f", f);
+    
+    f = sqrt(2);
+    NSLog(@"f: %f", f);
+    
+    f = M_SQRT2;
+    NSLog(@"f: %f", f);
+    
+    f = sqrt(81);
+    NSLog(@"f: %f", f);
+    
+    f = pow(3,3);
+    NSLog(@"f: %f", f);
+    
+    f = exp2(63) - 1;
+    NSLog(@"f: %f", f);
+    
+    
+    
 
     
     
